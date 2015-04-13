8d3af37c7efa74e2488caff3dec06b8adb75055c
German Martinez <germaaan@gmail.com>
2015-04-13 10:31:15 +0200

    Añadida comprobación afiliación UGR y selección de criterio de ordenación. Ref #1

--- ugrinvestiga.pl
+++ ugrinvestiga.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use v5.14;
 use utf8;
+use open IO => ':locale';
 
 use lib qw(../lib lib );
 
@@ -11,14 +12,25 @@ use Web::Scraper::Citations;
 use Mojo::ByteStream 'b';
 use File::Slurp qw(read_file write_file);
 
+my $opciones = "\n CRITERIO:\t 1 -> Citas totales" .
+  "\n\t\t 2 -> Citas en los últimos 5 años" .
+  "\n\t\t 3 -> Indice h total" .
+  "\n\t\t 4 -> Indice h de los últimos 5 años" .
+  "\n\t\t 5 -> Indice i10 total" .
+  "\n\t\t 6 -> Indice i10 de los últimos 5 años\n\n";
+
 my $num_args = $#ARGV;
-if ($num_args != 1){
-  die "Uso: $0 ENTRADA.dat SALIDA.csv " .
+if ($num_args != 2){
+  die "\nUso: $0 ENTRADA.dat SALIDA.csv CRITERIO" .
     "\n ENTRADA.dat -> archivo con los IDs en Google Scholar de los investigadores" .
-    "\n SALIDA.csv -> archivo con el ranking en formato CSV\n";
+    "\n SALIDA.csv -> archivo con el ranking en formato CSV" . $opciones;
 }
 
-my ($infile, $outfile) = @ARGV;
+my ($infile, $outfile, $mode) = @ARGV;
+
+if ($mode<1 or $mode>6){
+  die "\nCriterio de ordenación introducido inválido" . $opciones;
+}
 
 # Lectura del archivo de entrada con los IDs en Google Scholar de los investigadores
 my @researcher_ids = read_file($infile, chomp => 1);
@@ -34,21 +46,27 @@ foreach (@researcher_ids){
   }
   # Eliminación de todos los ",", ";" y "." del campo de afiliación
   $row[1] =~ s/[,;.]/ -/gm;
+
+  if ($row[1] !~ /Granada/){
+    push @row, "COMPROBAR";
+  }
+
   push @dataset, \@row;
 
   # Espera de 30 segundos antes de hacer la consulta de los datos de otro investigador
   sleep(30);
 }
 
-# Ordenación del listado en función del número de citas totales
-my @sorted = sort {$b->[2] <=> $a->[2]} @dataset;
+# Ordenación del listado en función del criterio seleccionado
+$mode = $mode + 1;
+my @sorted = sort {$b->[$mode] <=> $a->[$mode]} @dataset;
 
 @dataset = ();
 
 # Formateo de los datos de cada uno de los investigadores como valores separados por comas
 foreach (@sorted){
   my @person = @{$_};
-  push @dataset, join(", ", @person);
+  push @dataset, join(",", @person);
 }
 
 # Escritura del ranking en el archivo de salida
